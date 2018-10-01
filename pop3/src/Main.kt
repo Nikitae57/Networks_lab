013e470dcc4f6824efa1e218d60604f43cc093ea
Evdokimov Nikita <nikita-e57@yandex.ru>
2018-10-01 09:34:02 +0300

    Docs added

--- pop3/src/Main.kt
+++ pop3/src/Main.kt
@@ -1,4 +1,6 @@
-import java.io.*
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.io.PrintWriter
 import java.util.*
 import javax.net.ssl.SSLSocket
 import javax.net.ssl.SSLSocketFactory
@@ -23,7 +25,16 @@ class Main {
             var msg = socketReader.nextLine()
             while (true) {
                 println(msg)
-                msg = socketReader.nextLine()
+
+                if (msg.contains("signing off") ||
+                        msg.contains("ERR Authentication failed")) {
+
+                    System.exit(0)
+                }
+
+                if (socketReader.hasNext()) {
+                    msg = socketReader.nextLine()
+                }
             }
         }
 
@@ -41,9 +52,6 @@ class Main {
                 }
             }
         }
-
-
-
     }
 
     companion object Object {
@@ -53,4 +61,44 @@ class Main {
             m.start()
         }
     }
+
+    /*
+    USER <SP> <name> <CRLF>
+
+    PASS <SP> <passw> <CRLF>
+
+    QUIT <CRLF>
+
+    STAT <CRLF>
+      +OK <n> <s>
+          n - amount of emails, s - their size
+
+    IST [<SP> <mes>] <CRLF>
+      -ERR no such message
+
+      +OK scan listing follows
+      <n1> <s1>
+      <n2> <s2>...
+          n[i] - number of message, s[i] - size of message (bytes)
+
+    RETR <SP> <mes> <CRLF>
+      +OK message follows <email text>
+      -ERR no such message
+
+    DELE <SP> <mes> <CRLF>
+      +OK message deleted
+      -ERR no such message
+
+    NOOP <CRLF>
+      Checks server status
+
+    RSET <CRLF>
+      Removes delete flag from every email
+
+    TOP <SP> <mes> <SP> <n> <CRLF>
+        Returns header and first n lines of email
+
+    UIDL [<SP> <mes>] <CRLF>
+        Returns uid of email
+     */
 }

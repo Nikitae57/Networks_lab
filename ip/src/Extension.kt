8b33fab1c5e048f75673ac2328f0734c97baa8d7
Nikita Evdokimov <nikita-e57@yandex.ru>
2018-11-10 15:22:38 +0300

    Ip done

--- ip/src/Extension.kt
+++ ip/src/Extension.kt
@@ -1,4 +1,3 @@
-import java.io.IOException
 import java.util.*
 
 
@@ -28,10 +27,10 @@ fun BitSet.toDecStr(): String {
     return "$octet1.$octet2.$octet3.$octet4"
 }
 
-fun BitSet.from(value: Long, shift: Int = 0): BitSet {
+fun BitSet.from(value: Long, capacity: Int = 0): BitSet {
     val bits = BitSet()
-    val binaryStr = "0".repeat(shift) + Integer.toBinaryString(value.toInt())
-    println(binaryStr + " " + binaryStr.length)
+    val binStr = Integer.toBinaryString(value.toInt())
+    val binaryStr = "0".repeat(capacity - binStr.length) + binStr
     for (i in 0 until binaryStr.length) {
         if (binaryStr[i] == '1') {
             bits.set(i)
@@ -39,29 +38,6 @@ fun BitSet.from(value: Long, shift: Int = 0): BitSet {
     }
 
     return bits
-}
-
-class Extension {
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val bs = BitSet().from(1023, 24)
-        }
-    }
-}
-
-fun BitSet.toStr(): String {
-    val sb = StringBuilder()
-    for (i in 0 until size()) {
-        if (get(i)) {
-            sb.append(1)
-        } else {
-            sb.append(0)
-        }
-    }
-
-    return sb.toString()
 }
 
 fun BitSet.getToLong(fromIndex: Int, toIndex: Int): Long {
@@ -73,59 +49,13 @@ fun BitSet.getToLong(fromIndex: Int, toIndex: Int): Long {
     return sum
 }
 
-fun BitSet.increment(): BitSet {
-    val octets = longArrayOf(
+fun BitSet.getOctets(): LongArray {
+    return longArrayOf(
             getToLong(0, 7),
             getToLong(8, 15),
             getToLong(16, 23),
             getToLong(24, 31)
     )
-
-    for (i in 3 downTo 1) {
-        if (octets[i] < 254) {
-            octets[i]++
-            break
-        } else {
-            octets[i] = 0
-        }
-    }
-
-    set(0, 8, from(octets[0]))
-    set(8, 16, from(octets[1]))
-    set(16, 24, from(octets[2]))
-    set(24, 32, from(octets[3]))
-
-    return this
-}
-
-fun BitSet.set(fromIndex: Int, toIndex: Int, bitset: BitSet) {
-    var index = 0
-    for (i in fromIndex until toIndex) {
-        if (bitset.get(index++)) {
-            set(i)
-        } else {
-            set(i, false)
-        }
-    }
-}
-
-fun BitSet.shr(shiftDistance: Int): BitSet {
-
-    for (i in (size() - 1) downTo 1) {
-        if (i - shiftDistance < 0) { break }
-
-        if (get(i - shiftDistance)) {
-            set(i)
-        } else {
-            set(i, false)
-        }
-    }
-
-    for (i in 0 until shiftDistance) {
-        set(i, false)
-    }
-
-    return this
 }
 
 fun Scanner.ask(prompt: String): String {
--- ip/src/Main.kt
+++ ip/src/Main.kt
@@ -1,21 +1,14 @@
 import java.io.IOException
 import java.lang.System.exit
 import java.util.*
+import kotlin.collections.ArrayList
+
+fun main(args: Array<String>) {
+    Main().go()
+}
 
 
 class Main {
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            try {
-                Main().go()
-            } catch (ioex: IOException) {
-                println(ioex.message)
-                exit(1)
-            }
-        }
-    }
 
     @Throws(IOException::class)
     fun go() {
@@ -41,7 +34,7 @@ class Main {
         val freeBits = 32 - (totalBits)
         val maxPossibleHosts = 1.shl(freeBits) - 2
 
-        if (maxPossibleHosts < maxHostsNumber) {
+        if (maxPossibleHosts < maxHostsNumber || totalBits >= 32) {
             println("Impossible to create such network")
             exit(1)
         }
@@ -52,20 +45,22 @@ class Main {
                 "${maskBitSet.toBinStr()}\n" +
                 "${maskBitSet.toDecStr()}\n")
 
-        val endIndex = bitsForNetwork + bitsForSubnetworks
-        val subnetworkAddrBitset = BitSet()
-        subnetworkAddrBitset.set(0, bitsForNetwork)
+        val subnetworkAddresses = ArrayList<BitSet>()
+        (0 until (1L.shl(bitsForSubnetworks))).mapTo(subnetworkAddresses) {
+            BitSet().from(it, bitsForNetwork + bitsForSubnetworks)
+        }
 
-        for (i in 0 until (1L.shl(bitsForSubnetworks))) {
-            var bitCount = 0
-            while (1.shl(bitCount) < i) {
-                bitCount++
+        for (i in 0 until subnetworkAddresses.size - 1) {
+            val octets1 = subnetworkAddresses[i].getOctets()
+            val octets2 = subnetworkAddresses[i + 1].getOctets()
+
+            if (octets2[3] == 0L) {
+                println("${octets1[0]}.${octets1[1]}.${octets1[2]}.${octets1[3] + 1} - " +
+                        "${octets2[0]}.${octets2[1]}.${octets2[2]}.254")
+            } else {
+                println("${octets1[0]}.${octets1[1]}.${octets1[2]}.${octets1[3] + 1} - " +
+                        "${octets2[0]}.${octets2[1]}.${octets2[2]}.${octets2[3] - 2}")
             }
-
-            val bitset = BitSet().from(i, bitsForNetwork + bitsForSubnetworks - bitCount - 1)
-            println(bitset.toBinStr())
-//            subnetworkAddrBitset.set(bitsForNetwork, endIndex, bitset.get(bitsForNetwork, endIndex))
-//            println(subnetworkAddrBitset.toBinStr())
         }
     }
 }
